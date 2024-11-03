b7ada6411c39ee4753d1cff0565d5c23a3dd18af
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-11-03 06:58:25 -0300

    Revert "refactor: :fire: Remove unused code."
    This reverts commit c8bb1680ae34b5f6f58ac4a3d8cda54156c336ff.

--- src/models/entities/person.py
+++ src/models/entities/person.py
@@ -1,14 +1,20 @@
 from dataclasses import dataclass
 
+from src.models.entities.phone import Phone
+
+
 @dataclass
 class Person:
     name: str
     age: int
-    phone: int
+    phone: Phone
 
     def __post_init__(self):
         """Método chamado dentro do __init__ para validar os campos da classe Person.
-        
+
+        Raises:
+            BlankFieldError: exceção lançada quando um campo obrigatório não é preenchido.
+            FieldTypeError: exceção lançada quando um campo é preenchido com um tipo de dado inválido.
         """
         if not (self.name and self.age and self.phone):
             raise Exception('Todos os campos são obrigatórios!')
